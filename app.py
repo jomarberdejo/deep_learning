5f3723d80160d264ae9f8774c7a5199948f35676
jomar <berdejojomar@gmail.com>
2024-05-10 00:26:36 +0800

    make changes to training data

--- app.py
+++ app.py
@@ -7,9 +7,8 @@ CORS(app)
 
 @app.get("/")
 def index_get():
-    # return render_template("base.html")
-    return jsonify({"message": "Hello"})
-
+    return render_template("base.html")
+   
 @app.post("/predict")   
 def predict():
     text = request.get_json().get("message")
