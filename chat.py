4cde95b634afa60bdae73bac200d95aafa729f84
jomar <berdejojomar@gmail.com>
2024-05-17 12:48:12 +0800

    enhance bot training

--- chat.py
+++ chat.py
@@ -1,19 +1,17 @@
 import random
 import json
-
 import torch
-
 from model import NeuralNet
 from nltk_utils import bag_of_words, tokenize
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
 
 with open('intents.json', 'r') as json_data:
     intents = json.load(json_data)
 
 FILE = "data.pth"
 data = torch.load(FILE)
-
 input_size = data["input_size"]
 hidden_size = data["hidden_size"]
 output_size = data["output_size"]
@@ -21,13 +19,16 @@ all_words = data['all_words']
 tags = data['tags']
 model_state = data["model_state"]
 
+
 model = NeuralNet(input_size, hidden_size, output_size).to(device)
 model.load_state_dict(model_state)
 model.eval()
 
-bot_name = "Sam"
+
+unanswered_queries = []
 
 def get_response(msg):
+    global unanswered_queries
     sentence = tokenize(msg)
     X = bag_of_words(sentence, all_words)
     X = X.reshape(1, X.shape[0])
@@ -44,19 +45,13 @@ def get_response(msg):
         for intent in intents['intents']:
             if tag == intent["tag"]:
                 return random.choice(intent['responses'])
-    
-    return "My knowledge is currently limited to the capabilities of this application. Please provide more details so I can assist you better."
+    else:
+      
+        unanswered_queries.append(msg)
+        print("\nQueries for review:")
+        unique_queries = set(unanswered_queries)
+        for query in unique_queries:
+            print("- ", query)
+        return "My knowledge is currently limited. I've noted down your query for review."
 
-
-
-if __name__ == "__main__":
-    print("Let's chat! (type 'quit' to exit)")
-    while True:
-        # sentence = "do you use credit cards?"
-        sentence = input("You: ")
-        if sentence == "quit":
-            break
-
-        resp = get_response(sentence)
-        print(resp)
 
